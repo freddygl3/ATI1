247cd8b0047d2a8296901db37b12d890ef81f5ce
Freddy Lopez <freddygllz@gmail.com>
2025-05-08 17:43:40 -0400

    listado de estudiantes dinámico usando javascript

--- js/index.js
+++ js/index.js
@@ -1,13 +1,19 @@
 const configLink = document.getElementById('configES');
+const datosLink = document.getElementById('datosJson');
 
 // Función para cargar la configuración
 async function cargarConfiguracion() {
     try {
-        const response = await fetch(configLink.href);
-        const config = await response.json();
-        console.log('Configuración cargada:', config);
+        const [config, data] = await Promise.all([
+            fetch(configLink.href).then(r => r.json()),
+            fetch(datosLink.href).then(r => r.json())
+        ]);
+
+        console.log('Datos cargados:', { config, data });
+
         // Usar la configuración en tu aplicación
         cargarCfg(config);
+        cargarData(data);
 
     } catch (error) {
         console.error('Error cargando configuración:', error);
@@ -25,6 +31,34 @@ function cargarCfg(cfg){
 
 }
 
+function cargarData(data){
+    const lista_estudiantes = document.getElementsByClassName('lista-estudiantes')[0];
+
+    data.forEach(estudiante => {
+        const li = document.createElement('li');
+        li.className = 'estudiante';
+
+        const img = document.createElement('img');
+        img.src = estudiante.imagen;
+        img.alt = estudiante.nombre;
+
+        const p = document.createElement('p');
+        p.textContent = estudiante.nombre;
+
+        // Obtener la CI
+        const perfilID = estudiante.ci;
+        li.setAttribute("data-id", perfilID);
+
+        li.addEventListener("click", function () {
+            window.location.href = `perfil.html?perfil=${perfilID}`;
+        });
+
+        li.appendChild(img);
+        li.appendChild(p);
+        lista_estudiantes.appendChild(li);
+    });
+
+}
 // Ejecutar al cargar la página
 window.addEventListener('DOMContentLoaded', cargarConfiguracion);
 
